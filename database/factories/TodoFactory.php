75fed68fa01a9945506eddb532714808540db7e0
Ivan Brilata <brilataivan86@gmail.com>
2025-07-16 13:33:52 +0800

    php artisan seeder latest update

--- database/factories/TodoFactory.php
+++ database/factories/TodoFactory.php
@@ -6,15 +6,18 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TodoFactory extends Factory
 {
+    protected $model = \App\Models\Todo::class;
     /**
      * Define the model's default state.
      *
-     * @return array
+     * @return array<string, mixed>
      */
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph(4),
+            'completed' => false,
         ];
     }
 }
--- database/seeders/TodosSeeder.php
+++ database/seeders/TodosSeeder.php
@@ -3,16 +3,17 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Todo;
 
-class DatabaseSeeder extends Seeder
+class TodosSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(TodosSeeder::class);
+       Todo::factory()->count(10)->create();
     }
 }
