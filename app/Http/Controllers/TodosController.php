98030dcacb71010b3f2888031001ac4b79c77b12
Ivan Brilata <brilataivan86@gmail.com>
2025-07-20 23:38:52 +0800

    latest update

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
+
 use Illuminate\Http\Request;
 
 class TodosController extends Controller
 {
     public function index()
     {
-        return view('todos.index');
+        return view('todos.index')->with('todos', Todo::all());
     }
 }
